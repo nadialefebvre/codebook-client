7c98ba7b25f21caefab31ae1c3806087b147849d
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2023-06-26 22:44:29 +0200

    add text-editor

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import "bulmaswatch/superhero/bulmaswatch.min.css"
 import React from "react"
 import ReactDOM from "react-dom/client"
-import CodeCell from "./components/code-cell"
+// import CodeCell from "./components/code-cell"
+import TextEditor from "./components/text-editor"
 
 const App = () => {
   return (
     <>
-      <CodeCell />
+      {/* <CodeCell /> */}
+      <TextEditor />
     </>
   )
 }
