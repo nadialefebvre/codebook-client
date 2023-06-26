9f934725d36aa2fa82f6f325bd757eede14c80cd
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2023-06-26 09:42:49 +0200

    start with code editor

---
+++ src/components/code-editor.tsx
@@ -0,0 +1,7 @@
+import MonacoEditor from "@monaco-editor/react"
+
+const CodeEditor = () => {
+  return <MonacoEditor height="500px" language="javascript" theme="dark" />
+}
+
+export default CodeEditor
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect, useRef } from "react"
 import ReactDOM from "react-dom/client"
 import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin"
 import { fetchPlugin } from "./plugins/fetch-plugin"
+import CodeEditor from "./components/code-editor"
 
 const App = () => {
   const ref = useRef<any>()
@@ -39,7 +40,6 @@ const App = () => {
       },
     })
 
-    // setCode(result.outputFiles[0].text)
     iframe.current.contentWindow.postMessage(result.outputFiles[0].text, "*")
   }
 
@@ -65,6 +65,7 @@ const App = () => {
 
   return (
     <div>
+      <CodeEditor />
       <textarea
         value={input}
         onChange={(e) => setInput(e.target.value)}
