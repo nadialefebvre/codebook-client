7fa319092646fa6962266b9ef79f91d07c75f587
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2023-06-27 12:08:44 +0200

    link store to react

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
 import "bulmaswatch/superhero/bulmaswatch.min.css"
 import React from "react"
 import ReactDOM from "react-dom/client"
+import { Provider } from "react-redux"
 // import CodeCell from "./components/code-cell"
 import TextEditor from "./components/text-editor"
+import { store } from "./state"
 
 const App = () => {
   return (
-    <>
-      {/* <CodeCell /> */}
-      <TextEditor />
-    </>
+    <Provider store={store}>
+      <div>
+        {/* <CodeCell /> */}
+        <TextEditor />
+      </div>
+    </Provider>
   )
 }
 
