b49e229ffa8a68938510ec64035fbf7f80216c1d
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2023-06-27 17:16:12 +0200

    test actions with dispatch

--- src/state/store.ts
+++ src/state/store.ts
@@ -2,4 +2,34 @@ import { createStore, applyMiddleware } from "redux"
 import thunk from "redux-thunk"
 import reducers from "./reducers"
 
+import { ActionType } from "./action-types"
+
 export const store = createStore(reducers, {}, applyMiddleware(thunk))
+
+store.dispatch({
+  type: ActionType.INSERT_CELL_BEFORE,
+  payload: {
+    id: null,
+    type: "code",
+  },
+})
+
+store.dispatch({
+  type: ActionType.INSERT_CELL_BEFORE,
+  payload: {
+    id: null,
+    type: "text",
+  },
+})
+
+const id = store.getState().cells.order[0]
+
+store.dispatch({
+  type: ActionType.MOVE_CELL,
+  payload: {
+    id,
+    direction: "up",
+  },
+})
+
+console.log(store.getState())
