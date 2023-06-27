8e91596d2160149e44e8ec5e01d50c3061452e13
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2023-06-27 18:05:06 +0200

    add cell list and cell list item components

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,14 @@ import "bulmaswatch/superhero/bulmaswatch.min.css"
 import React from "react"
 import ReactDOM from "react-dom/client"
 import { Provider } from "react-redux"
-// import CodeCell from "./components/code-cell"
-import TextEditor from "./components/text-editor"
 import { store } from "./state"
+import CellList from "./components/cell-list"
 
 const App = () => {
   return (
     <Provider store={store}>
       <div>
-        {/* <CodeCell /> */}
-        <TextEditor />
+        <CellList />
       </div>
     </Provider>
   )
