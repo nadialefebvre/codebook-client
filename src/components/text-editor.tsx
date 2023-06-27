ce2a9c22e89bd00026bbaf36fec5cc9fdf57fe68
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2023-06-27 10:04:45 +0200

    fix some styling for text editor

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -5,7 +5,7 @@ import "./text-editor.css"
 
 const TextEditor: React.FC = () => {
   const [editing, setEditing] = useState(false)
-
+  const [value, setValue] = useState("# Header")
   const ref = useRef<HTMLDivElement | null>(null)
 
   useEffect(() => {
@@ -29,15 +29,17 @@ const TextEditor: React.FC = () => {
 
   if (editing) {
     return (
-      <div ref={ref}>
-        <MDEditor />
+      <div className="text-editor" ref={ref}>
+        <MDEditor value={value} onChange={(v) => setValue(v || "")} />
       </div>
     )
   }
 
   return (
-    <div onClick={() => setEditing(true)}>
-      <MDEditor.Markdown source={"# Header"} />
+    <div className="text-editor card" onClick={() => setEditing(true)}>
+      <div className="card-content">
+        <MDEditor.Markdown source={value} />
+      </div>
     </div>
   )
 }
